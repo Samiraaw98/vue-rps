3536ebff993ef4595b2e1e27e1ce1d3d8d3a2fe9
Swarsame <user@Users-MacBook-Air.local>
2022-04-25 07:19:39 -0600

    initial repo

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<template>
+ <div id="app">
+  
+   <nav>
+      <router-link to="/login">Log In</router-link> |
+       <router-link to="/game">Game</router-link>
+   </nav>
+  
+   
+ </div>
+
+   
+
+     
+</template>
+
+<!-- <script>
+// import HelloWorld from '@/components/HelloWorld.vue'
+//   export default {
+//     name : ""
+//     components: {
+//     LoginView,
+//      },
+    
+//   }
+// </script>
+-->
+
+
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #ffffff;
+  background-color:#222629;
+}
+
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+#form{
+ padding-left: 200px;
+ font: white;
+}
+</style>
+
+
+
+
---
+++ src/router/index.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import LoginView from '@/views/LoginView'
+import GameView from '@/views/GameView'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'login',
+    component: LoginView
+  },
+  {
+    path: '/game',
+    name: 'game',
+    component: GameView
+  },
+
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
